cee51036cd6fa599f136fa5f88a5447775c6135b
Samuel Jonas <samueljonas.dev@outlook.com>
2024-05-16 20:26:01 -0300

    first-commit

---
+++ src/components/header/index.tsx
@@ -0,0 +1,18 @@
+import styles from './header.module.css'
+import Logo from '../../assets/logo.svg'
+import { Link } from 'react-router-dom'
+
+export function Header(){
+    return(
+        <header className={styles.container} >
+
+            <Link to='/'>
+                <img 
+                    src={Logo}
+                    alt='Logo Cripto App'
+                    className="logo"
+                />           
+            </Link>
+        </header>
+    )
+}
---
+++ src/pages/notfound/index.tsx
@@ -0,0 +1,9 @@
+
+export function NotFound() {
+
+    return (
+        <div>
+            <h1> Página 404, esta página não existe... </h1>
+        </div>
+    )
+}
